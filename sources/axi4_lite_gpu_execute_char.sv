6f41b5937eb242d89069db7c5798ffdfb733cd50
hexadec <andris.cseh@gmail.com>
2026-03-29 14:20:33 +0200

    Added letters I-R to character ROM

--- sources/axi4_lite_gpu_execute_char.sv
+++ sources/axi4_lite_gpu_execute_char.sv
@@ -28,7 +28,7 @@ module axi4_lite_gpu_execute_char #(
 
 localparam CHAR_WIDTH = 8;
 localparam CHAR_HEIGHT = 8;
-localparam CHARACTER_COUNT = 8;
+localparam CHARACTER_COUNT = 18;
 
 enum logic [2:0] {  IDLE = 0,
                     BUSY_GETROW, 
