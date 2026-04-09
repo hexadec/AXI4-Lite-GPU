967692c8afe8852687e9cce8eba130a0c3ef7ae8
hexadec <andris.cseh@gmail.com>
2026-04-09 08:22:47 +0200

    Improve testbench for char ROM and ring buffer

--- testbench/tb_axi4_lite_gpu_char_rom.sv
+++ testbench/tb_axi4_lite_gpu_char_rom.sv
@@ -2,7 +2,7 @@ module tb_axi4_lite_gpu_char_rom;
 
 localparam CHAR_WIDTH = 8;
 localparam CHAR_HEIGHT = 8;
-localparam CHARACTER_COUNT = 8;
+localparam CHARACTER_COUNT = 95;
 
 logic clk = 0;
 
@@ -10,6 +10,10 @@ logic en = 0;
 logic [$clog2(CHARACTER_COUNT) - 1 : 0] char_code = 0;
 logic [$clog2(CHAR_HEIGHT) - 1 : 0] char_row_addr = 0;
 logic [CHAR_WIDTH - 1:0] char_row;
+
+int char_rom_fd;
+int row_idx;
+string line_content;
 
 axi4_lite_gpu_char_rom #(
     .CHAR_WIDTH(CHAR_WIDTH),
@@ -28,6 +32,18 @@ always #5 clk = ~clk;
 initial begin
     #10
     $display("ROM contents loaded from file");
+    char_rom_fd = $fopen("./font.mem", "r");
+    row_idx = 0;
+    while ($fscanf(char_rom_fd, "%s", line_content) == 1) begin
+        #10
+        en = 1;
+        char_code = row_idx / 8;
+        char_row_addr = row_idx % 8;
+        #10
+        en = 0;
+        assert(line_content.atobin() == char_row) else $display("Char ROM mismatch: Row: %s, Idx: %d, ROM content: %b", line_content, row_idx, char_row);
+        row_idx += 1;
+    end
     #10
     $finish();
 end
--- testbench/tb_axi4_lite_gpu_ring_buffer.sv
+++ testbench/tb_axi4_lite_gpu_ring_buffer.sv
@@ -19,6 +19,9 @@ logic [DATA_WIDTH - 1 : 0] decoder_write_data;
 logic decoder_write_processing_ok = 0;
 logic decoder_write_processing_done = 0;
 
+logic buffer_full;
+logic buffer_empty;
+
 axi4_lite_gpu_ring_buffer #(
     .ADDRESS_WIDTH(ADDRESS_WIDTH),
     .DATA_WIDTH(DATA_WIDTH),
@@ -38,7 +41,10 @@ axi4_lite_gpu_ring_buffer #(
     .decoder_write_address(decoder_write_address),
     .decoder_write_data(decoder_write_data),
     .decoder_write_processing_ok(decoder_write_processing_ok),
-    .decoder_write_processing_done(decoder_write_processing_done)
+    .decoder_write_processing_done(decoder_write_processing_done),
+    // Status
+    .buffer_full(buffer_full),
+    .buffer_empty(buffer_empty)
 );
 
 always #5 clk = ~clk;
@@ -47,6 +53,8 @@ initial begin
     rst_n = 0;
     #100
     rst_n = 1;
+    #10
+    assert(buffer_empty);
     for (int i = 0; i < BUFFER_SIZE + 1; i++) begin
         #10
         gpu_write_processing_start = 1;
@@ -56,14 +64,27 @@ initial begin
         gpu_write_processing_start = 0;
         gpu_write_address = 0;
         gpu_write_data = 0;
+        assert(!buffer_empty);
+        if (i >= BUFFER_SIZE - 1) begin
+            assert(buffer_full);
+        end
     end
+    assert(buffer_full);
     for (int i = 0; i < BUFFER_SIZE + 1; i++) begin
         #10
+        if (i < BUFFER_SIZE - 1) begin
+            assert(decoder_write_address == i << 4);
+            assert(decoder_write_data == i);
+        end
         decoder_write_processing_ok = 1;
         decoder_write_processing_done = 1;
         #10
         decoder_write_processing_ok = 0;
         decoder_write_processing_done = 0;
+        assert(!buffer_full);
+        if (i >= BUFFER_SIZE - 1) begin
+            assert(buffer_empty);
+        end
     end
     #100
     $finish;
